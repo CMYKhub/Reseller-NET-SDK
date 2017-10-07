f006683633b999f991d38e8bb4119758d8f6a62a
Scott Florisson <scott@srfsys.com>
2017-10-07 15:26:16 +0800

    Update paper properties

--- samples/CMYKhub.ResellerApi.Samples/Program.cs
+++ samples/CMYKhub.ResellerApi.Samples/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using CMYKhub.ResellerApi.Client;
 using CMYKhub.ResellerApi.Client.Manufacturing;
 using CMYKhub.ResellerApi.Client.Prepress;
 
@@ -19,34 +21,37 @@ namespace CMYKhub.ResellerApi.Samples
             }
             else
             {
-                var apiUrl = "https://hublink.api.cmykhub.com";
-                var resellerId = "9999";
-                var apiKey = "INSERT KEY HERE";
-                var clientFactory = new CMYKhub.ResellerApi.Client.HttpClientFactory();
-                var manufacturingClient = new HublinkManufacturingClient(clientFactory, apiUrl, resellerId, apiKey);
-                var prepressClient = new HublinkPrepressClient(clientFactory, apiUrl, resellerId, apiKey);
                 if (args[0] == "order-get" && args.Count() == 2)
                 {
-                    GetOrder(args[1]) ;
+                    GetOrder(args[1]);
                 }
                 else if (args[0] == "customer-get" && args.Count() == 2)
                 {
-                    var customerId = args[1];
-                    var customer = manufacturingClient.GetCustomerAsync(customerId).Result;
-                    Console.WriteLine($"Name: {customer.Name}");
-                    Console.WriteLine($"Contact: {customer.ContactName}");
+                    GetCustomer(args[1]);
                 }
                 else if (args[0] == "order-upload" && args.Count() >= 3)
                 {
-                    var orderId = args[1];
-                    var fileNames = args.Skip(2).ToArray();
-                    foreach (var file in fileNames)
-                        if (!File.Exists(file)) throw new Exception($"File not found: {file}");
-
-                    var order = manufacturingClient.GetOrderAsync(orderId).Result;
-                    var ppTask = prepressClient.UploadArtworkAsync(orderId, order.HubId, fileNames);
-                    ppTask.Wait();
-                    Console.WriteLine("Files Uploaded");
+                    UploadOrder(args[1], args.Skip(2).ToArray());
+                }
+                else if (args[0] == "products-list" && args.Count() >= 1)
+                {
+                    ListProducts(args.Length > 1 ? args[1] : null);
+                }
+                else if (args[0] == "papers-list" && args.Count() >= 1)
+                {
+                    ListPapers(args.Length > 1 ? args[1] : null);
+                }
+                else if (args[0] == "finishings-list" && args.Count() >= 1)
+                {
+                    ListFinishings(args.Length > 1 ? args[1] : null);
+                }
+                else if (args[0] == "price-product" && args.Count() >= 2)
+                {
+                    GetProductPrice(args[1]);
+                }
+                else if (args[0] == "price-booklet")
+                {
+                    GetBookletPrice();
                 }
                 else
                 {
@@ -60,24 +65,24 @@ namespace CMYKhub.ResellerApi.Samples
             Console.ReadKey();
         }
 
+        private static ClientSettings GetSettings()
+        {
+            var apiUrl = "http://localhost:50595";//"https://hublink.api.cmykhub.com";
+            var resellerId = "4926";// "9999";
+            var apiKey = "xBZBrw6mDuS1bAA8G9E4quTmOHSlAPbRuyewj+0ujnY=";// "INSERT KEY HERE";
+            return new ClientSettings(apiUrl, resellerId, apiKey);
+        }
+
         private static HublinkManufacturingClient GetManufacturingClient()
         {
-            var apiUrl = "https://hublink.api.cmykhub.com";
-            var resellerId = "9999";
-            var apiKey = "INSERT KEY HERE";
             var clientFactory = new CMYKhub.ResellerApi.Client.HttpClientFactory();
-            return new HublinkManufacturingClient(clientFactory, apiUrl, resellerId, apiKey);
-
+            return new HublinkManufacturingClient(clientFactory, GetSettings());
         }
 
         private static HublinkPrepressClient GetPrepressClient()
         {
-            var apiUrl = "https://hublink.api.cmykhub.com";
-            var resellerId = "9999";
-            var apiKey = "INSERT KEY HERE";
             var clientFactory = new CMYKhub.ResellerApi.Client.HttpClientFactory();
-            return new HublinkPrepressClient(clientFactory, apiUrl, resellerId, apiKey);
-
+            return new HublinkPrepressClient(clientFactory, GetSettings());
         }
 
         private static void GetOrder(string number)
@@ -90,10 +95,110 @@ namespace CMYKhub.ResellerApi.Samples
             Console.WriteLine($"Status: {order.StatusName}");
         }
 
+        private static void UploadOrder(string number, params string[] fileNames)
+        {
+            var manufacturingClient = GetManufacturingClient();
+            var prepressClient = GetPrepressClient();
+            foreach (var file in fileNames)
+                if (!File.Exists(file)) throw new Exception($"File not found: {file}");
+
+            var order = manufacturingClient.GetOrderAsync(number).Result;
+            var ppTask = prepressClient.UploadArtworkAsync(number, order.HubId, fileNames);
+            ppTask.Wait();
+            Console.WriteLine("Files Uploaded");
+        }
+
+        private static void GetCustomer(string number)
+        {
+            var manufacturingClient = GetManufacturingClient();
+            var customer = manufacturingClient.GetCustomerAsync(number).Result;
+            Console.WriteLine($"Name: {customer.Name}");
+            Console.WriteLine($"Contact: {customer.ContactName}");
+        }
+
+        private static void ListProducts(string name = null)
+        {
+            var manufacturingClient = GetManufacturingClient();
+            IEnumerable<Product> products;
+            if (!string.IsNullOrEmpty(name))
+                products = manufacturingClient.GetProductsByNameAsync(name).Result;
+            else
+                products = manufacturingClient.GetProductsAsync().Result;
+            foreach (var product in products)
+                Console.WriteLine($"{product.Id} : {product.Name}");
+        }
+
+        private static void ListFinishings(string name = null)
+        {
+            var manufacturingClient = GetManufacturingClient();
+            IEnumerable<Finishing> finishings;
+            if (!string.IsNullOrEmpty(name))
+                finishings = manufacturingClient.GetFinishingsByNameAsync(name).Result;
+            else
+                finishings = manufacturingClient.GetFinishingsAsync().Result;
+            foreach (var finishing in finishings)
+                Console.WriteLine($"{finishing.Id} : {finishing.Name}");
+        }
+
+        private static void ListPapers(string name = null)
+        {
+            var manufacturingClient = GetManufacturingClient();
+            IEnumerable<Paper> papers;
+            if (!string.IsNullOrEmpty(name))
+                papers = manufacturingClient.GetPapersByNameAsync(name).Result;
+            else
+                papers = manufacturingClient.GetPapersAsync().Result;
+            foreach (var paper in papers)
+                Console.WriteLine($"{paper.Id} : {paper.Name}");
+        }
+
+        private static void GetProductPrice(string productId)
+        {
+            var manufacturingClient = GetManufacturingClient();
+            var price = manufacturingClient.CreatePriceAsync(new StandardPriceRequest { ProductId = productId, Quantity = 1000, Kinds = 1 }).Result;
+
+            Console.WriteLine($"Ex Tax: {price.Price.ExTax}");
+            Console.WriteLine($"Expires: {price.Expires.ToString("dd MMM yyyy")}");
+        }
+
+        private static void GetBookletPrice()
+        {
+            var manufacturingClient = GetManufacturingClient();
+            var price = manufacturingClient.CreatePriceAsync(new BookletProductRequest
+            {
+                BindingId = "1",
+                Quantity = 1000,
+                Body = new BookletBodySection
+                {
+                    PaperId = "151",
+                    Pp = 32
+                },
+                FinishedSize = new Size { Width = 210, Height = 297 },
+                Orientation = 0,
+                PrintType = 1,
+            }).Result;
+
+            if (price == null)
+                Console.WriteLine("Unable to retrieve price");
+            else
+            {
+                Console.WriteLine($"Ex Tax: {price.Price.ExTax}");
+                Console.WriteLine($"Expires: {price.Expires.ToString("dd MMM yyyy")}");
+            }
+        }
+
         private static void PrintHelp()
         {
             Console.WriteLine("Options");
             Console.WriteLine("  order-get [orderId] : this will return the order information for the order with the given id");
+            Console.WriteLine("  order-upload [orderId] [filePaths]... : this will uploads the given files for the order with the given id");
+            Console.WriteLine("  customer-get [customerId] : this will return the customer information for the customer with the given id");
+            Console.WriteLine("  papers-list [name] : this will return papers optionally filtered by name");
+            Console.WriteLine("  finishings-list [name] : this will return finishings optionally filtered by name");
+            Console.WriteLine("  products-list [name] : this will return products optionally filtered by name");
+            Console.WriteLine("  price-product [productId] : this will return a price for the product with the given id");
+
+
             Console.WriteLine("");
         }
 
--- src/CMYKhub.ResellerApi.Client/Manufacturing/Paper.cs
+++ src/CMYKhub.ResellerApi.Client/Manufacturing/Paper.cs
@@ -3,6 +3,10 @@
     public class Paper : ApiResource
     {
         public string Id { get; set; }
-        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Family { get; set; }
+        public string Colour { get; set; }
+        public string Texture { get; set; }
+        public int Weight { get; set; }
     }
 }
