6d57b36d20ab23c9d9c6891ce917f7f78b62eef7
Scott Florisson <scott@srfsys.com>
2017-10-07 15:21:01 +0800

    Update resource properties

--- src/CMYKhub.ResellerApi.Client/Manufacturing/FinishingType.cs
+++ src/CMYKhub.ResellerApi.Client/Manufacturing/FinishingType.cs
@@ -1,6 +1,6 @@
 ﻿namespace CMYKhub.ResellerApi.Client.Manufacturing
 {
-    public class Paper : ApiResource
+    public class FinishingType
     {
         public string Id { get; set; }
         public string Name { get; set; }
